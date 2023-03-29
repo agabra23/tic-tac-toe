fa990b4f8aea0f2fe9854545c4b99857e6db6f87
Andrew Gabra <123777400+agabra23@users.noreply.github.com>
2023-03-29 13:45:37 -0400

    Setup basic gameboard module

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
 let boardArr = ["X", "O", "X", "O", "X", "O", "X", "O", "X"];
 
-const renderGrid = (boardArr) => {
-  const cells = document.querySelectorAll(".cell");
-  let index = 0;
-  cells.forEach((cell) => {
-    cell.textContent = boardArr[index];
-    index++;
-  });
-};
-
 const gameBoard = (function (boardArr) {
   this.boardArr = boardArr;
+
   this.resetBoard = () => {
     boardArr = ["", "", "", "", "", "", "", "", ""];
   };
-})();
+
+  this.renderGrid = (function (boardArr) {
+    console.log("working");
+    const cells = document.querySelectorAll(".cell");
+    cells.forEach((cell, i) => {
+      console.log(cell);
+      cell.innerHTML = boardArr[i];
+    });
+  })(boardArr);
+})(boardArr);
 
 const player = (name, icon) => {
   const sayHello = () => console.log("hello!");
   return { name, age, sayHello };
 };
-
-renderGrid();
