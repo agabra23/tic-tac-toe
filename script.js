ffbcad54ce963c38c42a366fd3526a1af288b5a1
Andrew Gabra <123777400+agabra23@users.noreply.github.com>
2023-03-29 15:31:12 -0400

    Create console logic for shell of the game

--- script.js
+++ script.js
@@ -1,1 +1,79 @@
 let boardArr = ["X", "O", "X", "O", "X", "O", "X", "O", "X"];
+
+// Gameboard Object
+function Gameboard() {
+  const rows = 3;
+  const columns = 3;
+  const board = [];
+
+  for (let i = 0; i < rows; i++) {
+    board[i] = [];
+    for (let j = 0; j < columns; j++) {
+      board[i].push(Cell());
+    }
+  }
+
+  const getBoard = () => board;
+
+  const placeIcon = (row, column, player) => {
+    board[row][column].addMark(player);
+  };
+
+  const printBoard = () => {
+    const boardWithCellValues = board.map((row) =>
+      row.map((cell) => cell.getValue())
+    );
+  };
+
+  return { getBoard, placeIcon, printBoard };
+}
+
+// Cell Object
+function Cell() {
+  let value = 0;
+
+  const addMark = (player) => {
+    value = player;
+  };
+
+  const getValue = () => value;
+
+  return { addMark, getValue };
+}
+
+function GameController(playerOneName = "User", playerTwoName = "Computer") {
+  const board = Gameboard();
+  const players = [
+    { name: playerOneName, token: "X" },
+    { name: playerTwoName, token: "O" },
+  ];
+
+  let activePlayer = players[0];
+
+  const switchPlayerTurn = () => {
+    activePlayer = activePlayer === players[0] ? players[1] : players[0];
+  };
+
+  const getActivePlayer = () => activePlayer;
+
+  const printNewRound = () => {
+    board.printBoard();
+    console.log(`${getActivePlayer().name}'s turn.`);
+  };
+
+  const playRound = (column) => {
+    board.placeIcon(column, row, getActivePlayer().token);
+
+    switchPlayerTurn();
+    printNewRound();
+  };
+
+  printNewRound();
+
+  return {
+    playRound,
+    getActivePlayer,
+  };
+}
+
+const game = GameController();
