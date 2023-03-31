e143cb2a1ffe77b9c59ab7b05f31ebba08640c74
Andrew Gabra <123777400+agabra23@users.noreply.github.com>
2023-03-31 12:47:22 -0400

    Style turn status text
    Change token array from 2D to 1D (easier win condition checking)
    
    Implement a checkWin() function checking win conditions.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # tic-tac-toe
-Creating a Tic-Tac-Toe game using HTML, vanilla CSS, and vanilla JS. My main goal here is to have as little global scope clutter as possible. I will be leaning heavy into factory functions and or modules. If I need ONE of something, I will use a module. If I need multiple of something, I will use factories. 
+
+Creating a Tic-Tac-Toe game using HTML, vanilla CSS, and vanilla JS. My main goal here is to have as little global scope clutter as possible. I will be leaning heavy into factory functions and or modules. If I need ONE of something, I will use a module. If I need multiple of something, I will use factories.
--- script.js
+++ script.js
@@ -9,27 +9,27 @@ function resetGame() {
 
 // Gameboard Object
 function Gameboard() {
-  const rows = 3;
-  const columns = 3;
+  //   const rows = 3;
+  //   const columns = 3;
+  const cells = 9;
   const board = [];
 
-  for (let i = 0; i < rows; i++) {
-    board[i] = [];
-    for (let j = 0; j < columns; j++) {
-      board[i].push(Cell());
-    }
+  for (let i = 0; i < cells; i++) {
+    board.push(Cell());
   }
 
+  // This will be the method of getting the entire board that our
+  // UI will eventually need to render it.
   const getBoard = () => board;
 
-  const placeIcon = (column, row, player) => {
-    board[row][column].addMark(player);
+  // Adds a player's token to a specific column and row.
+  const placeIcon = (cell, player) => {
+    board[cell].addMark(player);
   };
 
+  // prints the current 2D array onto the console by putting token values onto each spot
   const printBoard = () => {
-    const boardWithCellValues = board.map((row) =>
-      row.map((cell) => cell.getValue())
-    );
+    const boardWithCellValues = board.map((cell) => cell.getValue());
     console.log(boardWithCellValues);
   };
 
@@ -40,21 +40,56 @@ function Gameboard() {
 function Cell() {
   let value = "";
 
+  // changes value of the cell to the player's token
   const addMark = (player) => {
     value = player;
   };
 
+  // how we will get the current value
   const getValue = () => value;
 
   return { addMark, getValue };
 }
 
-function GameController(playerOneName = "User", playerTwoName = "Computer") {
+// will control game flow and win logic
+function GameController(
+  playerOneName = "Player 1",
+  playerTwoName = "Player 2"
+) {
   const board = Gameboard();
+  const boardArr = board.getBoard();
   const players = [
     { name: playerOneName, token: "X" },
     { name: playerTwoName, token: "O" },
   ];
+
+  const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  const checkWin = () => {
+    let roundWon = false;
+
+    for (let i = 0; i < winConditions.length; i++) {
+      const condition = winConditions[i];
+      const cellA = boardArr[condition[0]];
+      const cellB = boardArr[condition[1]];
+      const cellC = boardArr[condition[2]];
+
+      if (cellA == "" && cellB == "" && cellC == "") continue;
+      if (cellA == cellB && cellB == cellC) {
+        roundWon = true;
+        break;
+      }
+    }
+  };
 
   let activePlayer = players[0];
 
@@ -64,16 +99,23 @@ function GameController(playerOneName = "User", playerTwoName = "Computer") {
 
   const getActivePlayer = () => activePlayer;
 
+  // prints the board and displays player turn
   const printNewRound = () => {
     board.printBoard();
     console.log(`${getActivePlayer().name}'s turn.`);
   };
 
-  const playRound = (column, row) => {
-    board.placeIcon(column, row, getActivePlayer().token);
+  const playRound = (cell) => {
+    //places curr player's token into the array at the correct spot
+    board.placeIcon(cell, getActivePlayer().token);
 
-    switchPlayerTurn();
+    // Winner Logic
+
+    // if (checkWin) {
+
+    // }
     printNewRound();
+    switchPlayerTurn();
   };
 
   printNewRound();
@@ -96,29 +138,25 @@ function DisplayController() {
     const board = game.getBoard();
     const activePlayer = game.getActivePlayer();
 
-    playerTurnDisplay.textContent = `${activePlayer.name}'s turn...`;
+    playerTurnDisplay.innerHTML = `${activePlayer.name}'s turn...`;
 
-    board.forEach((row, i) => {
-      row.forEach((cell, j) => {
-        const cellButton = document.createElement("div");
-        cellButton.classList.add("cell");
+    board.forEach((cell, i) => {
+      const cellButton = document.createElement("div");
+      cellButton.classList.add("cell");
 
-        cellButton.dataset.column = j;
-        cellButton.dataset.row = i;
+      cellButton.dataset.cell = i;
 
-        cellButton.textContent = cell.getValue();
-        boardDiv.appendChild(cellButton);
-      });
+      cellButton.textContent = cell.getValue();
+      boardDiv.appendChild(cellButton);
     });
   };
 
   function clickHandlerBoard(e) {
-    const selectedColumn = e.target.dataset.column;
-    const selectedRow = e.target.dataset.row;
+    const selectedCell = e.target.dataset.cell;
 
-    if (!selectedColumn) return;
+    if (!selectedCell) return;
 
-    game.playRound(selectedColumn, selectedRow);
+    game.playRound(selectedCell);
     updateScreen();
   }
   boardDiv.addEventListener("click", clickHandlerBoard);
